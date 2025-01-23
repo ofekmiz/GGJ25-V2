71be0147fd102f45bc0c62f02f4d68ac0f5495aa
poopkinel <poopkinel@gmail.com>
2025-01-23 23:23:34 +0200

    PlayerController + Effect placeholder

--- Bubble/Assets/Domains/Core/GameManager.cs
+++ Bubble/Assets/Domains/Core/GameManager.cs
@@ -6,5 +6,22 @@ namespace Domains.Core
     public class GameManager : MonoBehaviour
     {
         [SerializeField] BubbleFactory _bubbleFactory;
+
+        [SerializeField] private PlayerController _playerPrefab;
+        [SerializeField] private Transform _playerParent;
+
+        private PlayerController _player;
+
+        public void CreatePlayer()
+        {
+            _player = Instantiate(_playerPrefab, _playerParent);
+            _player.PlayerDead += OnPlayerDead;
+        }
+
+        private void OnPlayerDead()
+        {
+            _player.PlayerDead -= OnPlayerDead;
+            Debug.Log($"Player Dead");
+        }
     }
 }
