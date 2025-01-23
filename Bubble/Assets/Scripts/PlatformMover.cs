b57282b2ef6f2c0ad410e6366b1521cf44d80d30
poopkinel <poopkinel@gmail.com>
2025-01-23 22:08:27 +0200

    Platformer initial commit

---
+++ Bubble/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    private Transform _trn;
+
+    
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        
+    }
+}
