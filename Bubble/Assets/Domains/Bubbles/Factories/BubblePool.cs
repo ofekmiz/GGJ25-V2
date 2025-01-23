7b001589d9592f5d4670160d2b0e0c6be6068e01
idanai <idany@tutanota.com>
2025-01-23 22:40:02 +0200

    Folders, GameManager and more

---
+++ Bubble/Assets/Domains/Bubbles/Factories/BubblePool.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Domains.Bubbles.BubbleEntity;
+
+namespace Domains.Bubbles.Factories
+{
+	public interface IBubblePool
+	{
+		
+	}
+
+	public class BubblePool
+	{
+		private List<IBubble> _pool;
+		
+	}
+}
