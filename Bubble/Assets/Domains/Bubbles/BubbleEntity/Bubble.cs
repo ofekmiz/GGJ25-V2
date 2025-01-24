30e8f22431e0e3eea1174548be34e35fe57914b3
idanai <idany@tutanota.com>
2025-01-24 12:00:12 +0200

    improved bubble spawner

--- Bubble/Assets/Domains/Bubbles/BubbleEntity/Bubble.cs
+++ Bubble/Assets/Domains/Bubbles/BubbleEntity/Bubble.cs
@@ -1,15 +1,26 @@
+using System;
 using Domains.Core;
 using UnityEngine;
 
 namespace Domains.Bubbles.BubbleEntity
 {
-
-    public interface IBubble
-    {
-    }
-
-    public class Bubble : MonoBehaviour, IBubble, IConstructed
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Bubble : MonoBehaviour, IConstructed
     {
         public void Construct(in Dependencies deps) { }
+
+        public Rigidbody2D Rigidbody { get; private set; }
+        public float SecondsAlive { get; set; }
+
+
+        private void Awake()
+        {
+            Rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            Rigidbody.gravityScale = 0;
+        }
     }
 }
