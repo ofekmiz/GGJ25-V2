c95314612d9652bb73ebc329d5c2b9ce978fe711
shirb <shirb@papaya.com>
2025-01-25 15:38:14 +0200

    Different sprites for good bad bubbles

--- Bubble/Assets/Domains/Bubbles/BubbleEntity/ModifierBubble.cs
+++ Bubble/Assets/Domains/Bubbles/BubbleEntity/ModifierBubble.cs
@@ -6,12 +6,19 @@ public class ModifierBubble : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _icon;
     
+    [SerializeField] private List<Sprite> _sprites;
+    [SerializeField] private List<Sprite> _badSprites;
+    [SerializeField] private SpriteRenderer _bubbleSprite;
+    
     private GameModifier _modifier;
     
     public void Set(GameModifier gameModifier)
     {
         _modifier = gameModifier;
         _icon.sprite = gameModifier.Icon;
+        
+        var randSprite = gameModifier.Good ? _sprites[Random.Range(0, _sprites.Count)] : _badSprites[Random.Range(0, _badSprites.Count)];
+        _bubbleSprite.sprite = randSprite;
     }
     
     public GameModifierType ModifierType => _modifier.Type;
