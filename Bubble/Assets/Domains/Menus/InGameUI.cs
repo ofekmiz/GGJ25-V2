f52d7063ce9e1356206f93257e30e7b2375a7d37
shirb <shirb@papaya.com>
2025-01-25 18:45:20 +0200

    ftue btn

---
+++ Bubble/Assets/Domains/Menus/InGameUI.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+    [SerializeField] private Button _ftueButton;
+
+    [SerializeField] private FTUEScreen _ftueScreen;
+
+    private void Awake()
+    {
+        _ftueButton.onClick.AddListener(()=>_ftueScreen.Show());
+    }
+}
